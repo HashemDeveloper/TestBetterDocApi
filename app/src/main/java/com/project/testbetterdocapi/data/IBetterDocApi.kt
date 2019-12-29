ed9fec3d9376e8b74e25e9c3565b4ac37489133a
Abul Hashem Jr <abul@limosys.com>
2019-12-29 18:03:37 -0500

    Setups api

--- app/src/main/java/com/project/testbetterdocapi/data/IBetterDocApi.kt
+++ app/src/main/java/com/project/testbetterdocapi/data/IBetterDocApi.kt
@@ -1,5 +1,15 @@
 package com.project.testbetterdocapi.data
 
-interface IBetterDocApi {
+import com.project.testbetterdocapi.models.BetterDocSearchByDiseaseRes
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+import retrofit2.http.Url
 
+interface IBetterDocApi {
+    @Headers("Content-Type: application/json")
+    @GET
+    suspend fun searchDocByDiseaseKtx(@Url url: String, @Query("user_key") userKey: String, @Query("limit")
+    limit: Int, @Query("location") location: String, @Query("query") disease: String, @Query("sort") sort: String): Response<BetterDocSearchByDiseaseRes>
 }
